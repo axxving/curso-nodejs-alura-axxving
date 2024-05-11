77a1e4e87c68deb71604ab5797b0130651963a27
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-11 13:46:40 -0600

    feat(): commenting the code

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// Importando la aplicación Express desde './src/app.js'
 import app from './src/app.js';
 
+// Definiendo el puerto en el que el servidor escuchará las solicitudes
 const PORT = 3000;
 
+// Iniciando el servidor Express y haciendo que escuche en el puerto especificado
 app.listen(PORT, () => {
+    // Imprimiendo un mensaje en la consola una vez que el servidor esté iniciado
     console.log('Servidor con express iniciado');
 });
--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 // Importando Express
 import express from 'express';
 
+// Creando una instancia de Express
 const app = express();
 
-app.get('/', ( req, res ) => {
+// Definiendo una ruta GET para el endpoint '/'
+app.get('/', (req, res) => {
+    // Respondiendo con un status 200 y enviando un mensaje
     res.status(200).send('Formacion Node + Express + MongoDb, usando Express');
 });
 
+// Exportando la aplicación Express para su uso en otros archivos
 export default app;
